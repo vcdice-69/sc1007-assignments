726ef70d4b861086fbbe8ae56e3b908caa0de1f3
vcdice-69 <160406447+vcdice-69@users.noreply.github.com>
2024-02-18 22:01:24 +0800

    Add files via upload

---
+++ sc1007 a2q2.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sc1007 a2q2
+//
+//  Created by Administrator on 18/2/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
